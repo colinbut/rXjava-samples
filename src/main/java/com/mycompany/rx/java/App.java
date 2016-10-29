8bd62e2e21a7a534911f91a03779d958ac3d68da
Colin But <colinbut@users.noreply.github.com>
2016-10-29 21:32:03 +0100

    cleaned up maven generated code

--- src/main/java/com/mycompany/rx/java/App.java
+++ src/main/java/com/mycompany/rx/java/App.java
@@ -2,12 +2,9 @@ package com.mycompany.rx.java;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
     }
 }
